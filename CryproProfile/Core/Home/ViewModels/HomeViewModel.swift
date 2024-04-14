8a10bcb0a19e058954374d4b57f1b64175c42998
Dima Kolesov <119799136+DmKolesov@users.noreply.github.com>
2024-04-14 14:11:29 +0300

    update Core Data

--- CryproProfile/Core/Home/ViewModels/HomeViewModel.swift
+++ CryproProfile/Core/Home/ViewModels/HomeViewModel.swift
@@ -17,6 +17,7 @@ class HomeViewModel: ObservableObject {
     
     private let coinDataService = CoinDataService()
     private let marketDataService = MarketDataService()
+    private let portfolioDataService = PortfolioDataService()
     private var cancellables = Set<AnyCancellable>()
     
     init() {
@@ -24,6 +25,7 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
+        
         $searchText
             .combineLatest(coinDataService.$allCoins)
             .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
@@ -39,7 +41,31 @@ class HomeViewModel: ObservableObject {
                 self?.statistics = returnedStats
             }
             .store(in: &cancellables)
+        
+        
+        $allCoins
+            .combineLatest(portfolioDataService.$savedEntities)
+            .map { (coinModels, portfolioEntities) -> [CoinModel] in
+                
+                coinModels
+                    .compactMap { coin -> CoinModel? in
+                        guard let entity = portfolioEntities.first(where: { $0.coinID == coin.id }) else { return nil
+                            
+                        }
+                        return coin.updateHoldings(amount: entity.amount)
+                        
+                    }
+            }
+            .sink { [weak self] returnedCoin in
+                self?.portfolioCoins = returnedCoin
+            }
+            .store(in: &cancellables)
     }
+    
+    func updatePortolio(coin: CoinModel, amount: Double) {
+        portfolioDataService.updatePortfolio(coin: coin, amount: amount)
+    }
+    
     
     private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
         guard !text.isEmpty else {
